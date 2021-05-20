f80213247656b9766606c029d3653ca0c1a2a6f0
Weslley L Silva <weslleylopes.dev@gmail.com>
2021-05-20 17:53:27 -0300

    Templates and models

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+class Category(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
+    slug = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Photo(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
+    category = models.ForeignKey(Category, on_delete=CASCADE)
+    image = models.ImageField(null=False, blank=False)
+    slug = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    
+
+    def __str__(self):
+        return str(self.name)
